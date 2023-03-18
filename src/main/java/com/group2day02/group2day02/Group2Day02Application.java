01bde92e75835a0eea62e747fc7dfe0e40515c0f
TW <tomasz.wojdynski@icloud.com>
2023-03-18 12:56:12 +0100

    Initial commit

---
+++ src/main/java/com/group2day02/group2day02/Group2Day02Application.java
@@ -0,0 +1,13 @@
+package com.group2day02.group2day02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group2Day02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group2Day02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/group2day02/group2day02/Group2Day02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2day02.group2day02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2Day02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
