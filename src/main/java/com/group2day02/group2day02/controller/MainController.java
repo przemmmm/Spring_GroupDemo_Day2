32569b92126a6e1a65bd074425416d9fe7ba28b1
TW <tomasz.wojdynski@icloud.com>
2023-03-18 13:05:28 +0100

    add main controller and start page

---
+++ src/main/java/com/group2day02/group2day02/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.group2day02.group2day02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/tasks")
+    public String getTaskPage() {
+        return "task-main-page";
+    }
+
+}
