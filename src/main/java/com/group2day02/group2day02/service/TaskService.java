efeff99494e9289cf3df02d15c55c2cb59bd8d86
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-03-19 10:51:58 +0100

    fix bugs

--- src/main/java/com/group2day02/group2day02/service/TaskService.java
+++ src/main/java/com/group2day02/group2day02/service/TaskService.java
@@ -1,12 +1,19 @@
 package com.group2day02.group2day02.service;
 
 import com.group2day02.group2day02.exception.UserServiceException;
+import com.group2day02.group2day02.repository.TaskRepository;
 import com.group2day02.group2day02.request.TaskCreationRequest;
 import com.group2day02.group2day02.request.UserCreationRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     public void addTask(TaskCreationRequest taskCreationRequest) {
         if (taskCreationRequest.getTitle().isBlank()) {
